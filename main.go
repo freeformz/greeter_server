61a5a6fcb3d36ccf0951f44c3ddd49b091958719
Edward Muller <edwardam@interlix.com>
2016-05-20 10:03:07 -0700

    No PORT set

--- main.go
+++ main.go
@@ -64,7 +64,6 @@ func getPort(e string) string {
 }
 
 func main() {
-	port := getPort("PORT")
 	httpPort := getPort("HEROKU_ROUTER_HTTP_PORT")
 	//httpsPort := getPort("HEROKU_ROUTER_HTTPS_PORT")
 	healthPort := getPort("HEROKU_ROUTER_HEALTHCHECK_PORT")
@@ -77,13 +76,6 @@ func main() {
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &server{})
 	go s.Serve(&proxyproto.Listener{lis})
-
-	go http.ListenAndServe(":"+port, http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			w.Write([]byte("From $PORT"))
-			w.WriteHeader(http.StatusOK)
-		}),
-	)
 
 	http.ListenAndServe(":"+healthPort, http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
